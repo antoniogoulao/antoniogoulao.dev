e8408e434dc725e0ad8440b35d0f7638fa17c006
Antonio Goulao <antoniomgoulao@gmail.com>
2026-04-20 23:39:18 +0100

    fix: add next-intl plugin, trailingSlash, fix gitignore and newlines

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
+import createNextIntlPlugin from 'next-intl/plugin';
 import type {NextConfig} from 'next';
+
+const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'docs',
+  trailingSlash: true,
   images: {unoptimized: true},
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
